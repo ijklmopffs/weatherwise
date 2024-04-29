307c24c3cec7aa9daf3f148f2c523f93c3878797
Nuel Elekwachi <elekwaeke300@gmail.com>
2024-04-29 01:24:55 +0100

    search component setup

---
+++ components/SearchBox.tsx
@@ -0,0 +1,11 @@
+export default function SearchBox() {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="City name"
+        className="bg-gray-200 p-2 rounded-lg w-64"
+      />
+    </>
+  );
+}
